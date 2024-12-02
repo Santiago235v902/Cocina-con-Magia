87f7643fe59a9617b785e22e0dc8c266a0a059c1
Benjamin <153400127+Santiago235v902@users.noreply.github.com>
2024-12-02 00:55:00 -0300

    Update script.js

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,13 +1,1 @@
-
-    // Seleccionar el elemento
-    const titulo = document.querySelector('.titulo-brillante');
-
-    // Escuchar eventos de mouse y táctil
-    titulo.addEventListener('mouseenter', () => detenerAnimacion());
-    titulo.addEventListener('click', () => detenerAnimacion());
-    titulo.addEventListener('touchstart', () => detenerAnimacion());
-
-    // Función para detener la animación
-    function detenerAnimacion() {
-        titulo.classList.add('paused');
-    }
+//EL SCRIPT NO FUNCIONA SE REMPLAZA FUNCION DIRECTAMENTE EN CSS
